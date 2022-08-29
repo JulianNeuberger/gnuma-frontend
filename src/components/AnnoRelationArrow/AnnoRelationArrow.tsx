8af48a4700b09ce1fd86edd76c57cb69375653c4
Bastian Völkl <flxt@pm.me>
2022-08-29 17:42:56 +0200

    New machine

--- src/components/AnnoRelationArrow/AnnoRelationArrow.tsx
+++ src/components/AnnoRelationArrow/AnnoRelationArrow.tsx
@@ -16,11 +16,11 @@ export default function AnnoRelationArrow(props: AnnoRelationArrowProps) {
             <Xarrow 
                 start={props.rel.subject.sentenceId + '_' + props.rel.subject.tokenId} 
                 end={props.rel.object.sentenceId + '_' + props.rel.object.tokenId}
-                startAnchor={[{position: 'left', offset: {y: 10}}, { position: 'right', offset: {y: 10}}, {position: 'top', offset: {x: 5}}, {position: 'bottom', offset: {x: 5}}]}
+                startAnchor={[{position: 'top', offset: {x: 5}}, {position: 'bottom', offset: {x: 5}}]}
                 endAnchor={[{position: 'left', offset: {y: 10}}, { position: 'right', offset: {y: 10}}, {position: 'top', offset: {x: -5}}, {position: 'bottom', offset: {x: -5}}]}
                 strokeWidth= {2}
                 headSize={4}
-                path={'straight'}  
+                path={'smooth'}
                 color = {props.color}
                 labels= {<span style= {{color: 'black' }}></span>}
             />
