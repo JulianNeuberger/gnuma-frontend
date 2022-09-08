9fbbd2a7aad2293a066f9febf321f5f61b697d1d
Bastian Völkl <flxt@pm.me>
2022-09-08 17:58:53 +0200

    Revamped Relation Arrows a bit.

--- src/components/AnnoRelationArrow/AnnoRelationArrow.tsx
+++ src/components/AnnoRelationArrow/AnnoRelationArrow.tsx
@@ -13,14 +13,26 @@ export default function AnnoRelationArrow(props: AnnoRelationArrowProps) {
 
     return(
         <>
+            <Xarrow
+                start={props.rel.subject.sentenceId + '_' + props.rel.subject.tokenId}
+                end={props.rel.subject.sentenceId + '_' + props.rel.subject.tokenId}
+                startAnchor={[{position: 'bottom', offset: {}}]}
+                endAnchor={[{position: 'bottom', offset: {y: 10}}]}
+                strokeWidth= {2}
+                headSize={4}
+                path={'straight'}
+                showHead={false}
+                color = {props.color}
+                labels= {<span style= {{color: 'black' }}></span>}
+            />
             <Xarrow 
                 start={props.rel.subject.sentenceId + '_' + props.rel.subject.tokenId} 
                 end={props.rel.object.sentenceId + '_' + props.rel.object.tokenId}
-                startAnchor={[{position: 'top', offset: {x: 5}}, {position: 'bottom', offset: {x: 5}}]}
-                endAnchor={[{position: 'left', offset: {y: 10}}, { position: 'right', offset: {y: 10}}, {position: 'top', offset: {x: -5}}, {position: 'bottom', offset: {x: -5}}]}
+                startAnchor={[{position: 'bottom', offset: {y: 9}}]}
+                endAnchor={[{position: 'left', offset: {y: 10}}, { position: 'right', offset: {y: 10}}, {position: 'top', offset:{}}, {position: 'bottom', offset:{}}]}
                 strokeWidth= {2}
                 headSize={4}
-                path={'smooth'}
+                path={'straight'}
                 color = {props.color}
                 labels= {<span style= {{color: 'black' }}></span>}
             />
